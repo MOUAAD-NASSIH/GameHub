9ac56cb5c1f99e7afc55682c9a3f27e5eb2987b5
MOUAAD-NASSIH <mouadnassih11@gmail.com>
2024-09-16 19:37:24 +0100

    Build the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Container, Grid, GridItem, Show } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`, // > 1024px
-      
-      }}
-    >
-      <GridItem area="nav">
-        <Navbar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          Aside
+    <Container maxWidth="container.xl" padding={0}>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`, // > 1024px
+        }}
+      >
+        <GridItem area="nav">
+          <Navbar />
         </GridItem>
-      </Show>
-      <GridItem area="main" bg="dodgerblue">
-        Main
-      </GridItem>
-    </Grid>
+        <Show above="lg">
+          <GridItem area="aside">Aside</GridItem>
+        </Show>
+        <GridItem area="main">Main</GridItem>
+      </Grid>
+    </Container>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 // import Epiclogo from "../assets/Epic_Games_logo.png";
 
 function Navbar() {
   return (
-    <HStack>
+    <HStack justifyContent="space-between" paddingInline="10px">
       <Image src={logo} boxSize="60px" />
       {/* <Image src={Epiclogo} height="40px" marginInline="10px" /> */}
-      <Text>Navbar</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 }
