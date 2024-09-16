2b472ca5b38223d1119b9cefda81db0c6a6bca13
MOUAAD-NASSIH <mouadnassih11@gmail.com>
2024-09-16 21:41:26 +0100

    create a custom Hook to fetch the games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,46 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
+import useGames from "../hooks/useGames";
 import { Text } from "@chakra-ui/react";
 
-// export interface EsrbRating {
-//     id:   number;
-//     slug: string;
-//     name: string;
-// }
-
-// export interface Platform {
-//     platform:     EsrbRating;
-//     released_at:  string;
-// }
-
-interface Game {
-  id: number;
-  // slug:               string;
-  name: string;
-  // released:           string;
-  // background_image:   string;
-  // rating:             number;
-  // esrb_rating:        EsrbRating;
-  // platforms:          Platform[];
-}
-
-export interface FetchGameResponse {
-  count: number;
-  // next:     string;
-  // previous: string;
-  results: Game[];
-}
-
 function GameGrid() {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGameResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  });
+  const { games, error } = useGames();
 
   return (
     <>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import axios, { AxiosError } from "axios";
+
+// export interface EsrbRating {
+//     id:   number;
+//     slug: string;
+//     name: string;
+// }
+
+// export interface Platform {
+//     platform:     EsrbRating;
+//     released_at:  string;
+// }
+
+export interface Game {
+  id: number;
+  // slug:               string;
+  name: string;
+  // released:           string;
+  // background_image:   string;
+  // rating:             number;
+  // esrb_rating:        EsrbRating;
+  // platforms:          Platform[];
+}
+
+export interface FetchGameResponse {
+  count: number;
+  // next:     string;
+  // previous: string;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGameResponse>("/games", { signal: controller.signal })
+      .then((res) => setGames(res.data.results))
+      .catch((err: AxiosError) => {
+        if (axios.isCancel(err)) return;
+
+        // Type assertion to handle AxiosError and other possible errors
+        const axiosError = err as AxiosError;
+        setError(axiosError.message || "An unknown error occurred");
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+
+export default useGames;
